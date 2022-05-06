deda369e659f872e01270da02c6471da50251fe6
Phil Berhane <pberhane@ks190838.attlocal.net>
2022-05-06 00:03:00 -0700

    form submission/validation + styling complete

--- express-template/index.js
+++ express-template/index.js
@@ -30,26 +30,25 @@ function parseSupervisors(data) {
   return formattedSupervisors;
 }
 
-app.get("/supervisors", async (req, res) => {
+app.get("/supervisors", (req, res) => {
   request(
     "https://o3m5qixdng.execute-api.us-east-1.amazonaws.com/api/managers",
-    function (error, response, body) {
+    function (error, response) {
       if (!error && response.statusCode == 200) {
         res.send(parseSupervisors(response.body));
+      } else {
+          res.send({error:"Sorry, your request cannot be processed at this time. Please try again later."})
       }
     }
   );
 });
 
-app.post("/echo", async (req, res) => {
-  try {
-    res.json({
-      body: req.body,
-    });
-  } catch (e) {
-    res.json({
-      error: e.message,
-    });
+app.post("/submit", (req, res) => {
+  if (req.body.firstName.length === 0 || req.body.lastName.length === 0 || req.body.supervisor.length === 0 || req.body.supervisor === "Select...") {
+      res.send({error:"Please fill all of the required fields."})
+  } else {
+      console.log(req.body.firstName, req.body.lastName, req.body.email, req.body.phoneNumber, req.body.supervisor)
+      res.send({success:"Your submission has been completed!"})
   }
 });
 
--- react-template/src/NotificationForm.js
+++ react-template/src/NotificationForm.js
@@ -7,9 +7,60 @@ export const NotificationForm = () => {
   const [emailCheckBox, setEmailCheckBox] = useState(false);
   const [phoneCheckBox, setPhoneCheckBox] = useState(false);
   const [email, setEmail] = useState("");
-  const [phone, setPhone] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
   const [supervisors, setSupervisors] = useState([]);
-  const [supervisor, setSupervisor] = useState("Select...");
+  const [supervisor, setSupervisor] = useState("");
+  const [serverMsg, setServerMsg] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (
+      firstName.length === 0 ||
+      lastName.length === 0 ||
+      supervisor.length === 0 ||
+      supervisor === "Select..."
+    ) {
+      setServerMsg("Please fill all of the required fields.");
+    } else {
+      fetch("http://localhost:8080/submit", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          firstName,
+          lastName,
+          email,
+          phoneNumber,
+          supervisor,
+        }),
+      })
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          if (data.error) {
+            setServerMsg(data.error);
+          }
+          if (data.success) {
+            setFirstName("");
+            setLastName("");
+            setEmailCheckBox(!emailCheckBox);
+            setPhoneCheckBox(false);
+            setPhoneNumber("");
+            setEmail("");
+            setSupervisor("Select...");
+            setServerMsg(data.success);
+          }
+        })
+        .catch((error) => {
+          setServerMsg(
+            "Sorry, your request cannot be processed at this time. Please try again later."
+          );
+        });
+    }
+  };
 
   useEffect(() => {
     fetch("http://localhost:8080/supervisors")
@@ -17,85 +68,110 @@ export const NotificationForm = () => {
         return response.json();
       })
       .then((data) => {
-        setSupervisors(data);
+        if (data.error) {
+          setServerMsg(data.error);
+        } else {
+          setSupervisors(data);
+        }
       })
       .catch((error) => {
-        console.log(error);
+        setServerMsg(
+          "Sorry, your request cannot be processed at this time. Please try again later."
+        );
       });
   }, []);
 
   return (
     <div className="form">
-      <h1 className="notificationHeader">Notification Form</h1>
-      <form>
-        <p>
-          <label>First Name</label>
-          <input
-            type="text"
-            requiredvalue={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-          />
-        </p>
-        <p>
-          <label>Last Name</label>
-          <input
-            type="text"
-            requiredvalue={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-          />
-        </p>
+      <div className="notificationHeader">
+        <h1>Notification Form</h1>
+      </div>
+      <form
+        onSubmit={(e) => {
+          handleSubmit(e);
+        }}
+      >
+        <div className="row firstRow">
+          <div className="column">
+            <label>First Name*</label>
+            <input
+              type="text"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+          </div>
+          <div className="column">
+            <label>Last Name*</label>
+            <input
+              type="text"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            />
+          </div>
+        </div>
         <h4 className="notificationPreference">
           How would you prefer to be notified?
         </h4>
-        <p>
-          <input
-            type="checkbox"
-            value={emailCheckBox}
-            onChange={(e) => setEmailCheckBox(e.target.value)}
-          />
-          <label>Email</label>
-        </p>
-        <p>
-          <input
-            type="checkbox"
-            id="phoneCheckBox"
-            name="phoneCheckBox"
-            value={phoneCheckBox}
-            onChange={(e) => setPhoneCheckBox(e.target.value)}
-          />
-          <label className="phoneCheckBoxLabel">Phone Number</label>
-        </p>
-        <p>
-          <input
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </p>
-        <p>
-          <input
-            type="text"
-            value={phone}
-            onChange={(e) => setPhone(e.target.value)}
-          />
-        </p>
-        <p className="formLower">
-          <label>Supervisor</label>
-          <select
-            className="dropdown"
-            value={supervisor}
-            onChange={(e) => setSupervisor(e.currentTarget.value)}
-          >
-            <option>Select...</option>
-            {supervisors.map((s) => (
-              <option key={s} value={s}>
-                {s}
-              </option>
-            ))}
-          </select>
-        </p>
-        <button>Submit</button>
+        <div className="row">
+          <div className="column">
+            <label className="checkBoxLabel">
+              <input
+                type="checkbox"
+                className="checkBox"
+                value={emailCheckBox}
+                checked={emailCheckBox}
+                onChange={(e) => setEmailCheckBox(!emailCheckBox)}
+              />
+              &nbsp; Email
+            </label>
+          </div>
+          <div className="column">
+            <label className="checkBoxLabel">
+              <input
+                type="checkbox"
+                className="checkBox"
+                name="phoneCheckBox"
+                value={phoneCheckBox}
+                checked={phoneCheckBox}
+                onChange={(e) => setPhoneCheckBox(!phoneCheckBox)}
+              />
+              &nbsp; Phone Number
+            </label>
+          </div>
+        </div>
+        <div className="row">
+          <div className="column">
+            <input
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="column">
+            <input
+              type="text"
+              value={phoneNumber}
+              onChange={(e) => setPhoneNumber(e.target.value)}
+            />
+          </div>
+        </div>
+        <label class="supervisor">Supervisor*</label>
+        <select
+          className="dropdown"
+          value={supervisor}
+          onChange={(e) => setSupervisor(e.currentTarget.value)}
+        >
+          <option>Select...</option>
+          {supervisors.map((s) => (
+            <option key={s} value={s}>
+              {s}
+            </option>
+          ))}
+        </select>
+        <br></br>
+        <button>SUBMIT</button>
       </form>
+      <p>{serverMsg}</p>
     </div>
   );
 };
